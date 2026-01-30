40452186c63df5d2ca8928e337671f88b3fe638c
Sharanya Mattaparthy <47758789+smattaparthy@users.noreply.github.com>
2026-01-30 13:01:24 -0500

    Remove EmployeesController and EF commands
    Removed Entity Framework commands and EmployeesController implementation.

--- EmployeeWasmApp.Api/Controller/EmployeesController.cs
+++ EmployeeWasmApp.Api/Controller/EmployeesController.cs
@@ -46,58 +46,10 @@ public class Employee
     public DateTime? SeparationDate { get; set; }
 }
 
+$proj = ".\EmployeeWasmApp.Infrastructure\EmployeeWasmApp.Infrastructure.csproj"
 
+dotnet add $proj package Microsoft.EntityFrameworkCore --version "10.*"
+dotnet add $proj package Microsoft.EntityFrameworkCore.Relational --version "10.*"
+dotnet add $proj package Microsoft.EntityFrameworkCore.Design --version "10.*"
+dotnet add $proj package Microsoft.EntityFrameworkCore.Sqlite --version "10.*"
 
-
-dotnet ef migrations add InitialCreate `
-  --project .\EmployeeWasmApp.Infrastructure\EmployeeWasmApp.Infrastructure.csproj `
-  --startup-project .\EmployeeWasmApp.Api\EmployeeWasmApp.Api.csproj `
-  --context EmployeeWasmApp.Infrastructure.Data.AppDbContext
-
-
-
-
-dotnet ef database update `
-  --project .\EmployeeWasmApp.Infrastructure\EmployeeWasmApp.Infrastructure.csproj `
-  --startup-project .\EmployeeWasmApp.Api\EmployeeWasmApp.Api.csproj `
-  --context EmployeeWasmApp.Infrastructure.Data.AppDbContext
-
-
-
-
-
-using EmployeeWasmApp.Infrastructure.Entities;
-using EmployeeWasmApp.Infrastructure.Repositories;
-using Microsoft.AspNetCore.Mvc;
-
-namespace EmployeeWasmApp.Api.Controllers;
-
-[ApiController]
-[Route("api/[controller]")]
-public class EmployeesController : ControllerBase
-{
-    private readonly IEmployeeRepository _repo;
-
-    public EmployeesController(IEmployeeRepository repo)
-    {
-        _repo = repo;
-    }
-
-    [HttpGet]
-    public Task<List<Employee>> GetAll(CancellationToken ct)
-        => _repo.GetAllAsync(ct);
-
-    [HttpGet("{id:guid}")]
-    public async Task<ActionResult<Employee>> GetById(Guid id, CancellationToken ct)
-    {
-        var emp = await _repo.GetByIdAsync(id, ct);
-        return emp is null ? NotFound() : emp;
-    }
-
-    [HttpPost]
-    public async Task<ActionResult<Employee>> Create(Employee employee, CancellationToken ct)
-    {
-        var created = await _repo.AddAsync(employee, ct);
-        return Ok(created);
-    }
-}
